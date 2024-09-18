604cfffb2b9c94516ed362d05fda20c11fb9acc4
Joel Blanco <joelb5700@gmail.com>
2024-09-18 11:00:38 +0200

    hecho

---
+++ ej2s.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
+<BODY>
+<?php
+    $ip="10.33.4.2";
+    for ($i=0; $i <4 ; $i++) 
+    { 
+        
+    
+        while (oct)
+        {
+
+        }
+    }
+?>
+</BODY>
+</HTML>

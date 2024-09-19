e0f7e17173437a7f9182322fb1928c87d745e7a2
Joel Blanco <joelb5700@gmail.com>
2024-09-19 13:33:17 +0200

    DONE

--- ej2s.php
+++ ej2s.php
@@ -2,13 +2,17 @@
 <HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
 <BODY>
 <?php
-    $ip = {10,33,4,2};
+    $ip = array(10,33,4,2);
+    $ipFinal = " ";
     for ($i=0; $i <4 ; $i++) 
     { 
         $octal = $ip[$i];
-        decbin(int $octal)
-
+        (String) $octalSinCeros = decbin($octal) ;
+        while (strlen($octalSinCeros) < 8)
+        $octalSinCeros = "0" . $octalSinCeros;
+        $ipFinal = $ipFinal . "." . $octalSinCeros;
     }
+    echo "La ip 10.33.4.2 es " . substr($ipFinal,2,strlen($ipFinal));
 ?>
 </BODY>
 </HTML>

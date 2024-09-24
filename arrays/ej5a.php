e64496017573774e4e5e693f796e6c57adc0f5f8
Joel Blanco <joelb5700@gmail.com>
2024-09-24 09:44:42 +0200

    ej5 A  hecho

--- arrays/ej5a.php
+++ arrays/ej5a.php
@@ -11,7 +11,7 @@
     $nombre = "array";
     $i = 0;
     while ($i < (count($array0)+count($array1)+count($array2))) {
-        $arrayAux = ($i < 3) ? ${$nombre.'0'} : (($i < 6) ? ${$nombre.'1'} : ${$nombre.'2'});
+        $arrayAux = ($i < 3) ? ${$nombre.'0'} : (($i <= 5) ? ${$nombre.'1'} : ${$nombre.'2'});
         $j = 0;
         while ($j < count($arrayAux))
         {
@@ -20,9 +20,6 @@
             $i += 1;  
         }   
     }
-
-
-    
     foreach ($arrayCompleta as $i ) 
         echo $i . "<br>";
     

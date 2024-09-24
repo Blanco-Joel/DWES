3c5ca901d9a658d7802ce11382a8f85d477ce6e2
Joel Blanco <joelb5700@gmail.com>
2024-09-24 09:50:24 +0200

    ej5   hecho

--- arrays/ej5a.php
+++ arrays/ej5a.php
@@ -8,6 +8,8 @@
     $array1 = array("Sistemas Informáticos","FOL","Mecanizado");
     $array2 = array("Desarrollo Web ES","Desarrollo Web EC","Despliegue","Desarrollo Interfaces", "Inglés");
     $arrayCompleta = array();
+    $arrayCompleta2  = array();
+    $arrayCompleta3  = array();
     $nombre = "array";
     $i = 0;
     while ($i < (count($array0)+count($array1)+count($array2))) {
@@ -22,8 +24,24 @@
     }
     foreach ($arrayCompleta as $i ) 
         echo $i . "<br>";
-    
 
+    /*********************************************************************************************/
+    $arrayCompleta2  = array_merge($array0,$array1,$array2);
+    foreach ($arrayCompleta2 as $i ) 
+        echo $i . "<br>";
+    /*********************************************************************************************/
+    $i = 0;
+    while ($i < (count($array0)+count($array1)+count($array2))) {
+        $arrayAux = ($i < 3) ? ${$nombre.'0'} : (($i <= 5) ? ${$nombre.'1'} : ${$nombre.'2'});
+        $j = 0;
+        while ($j < count($arrayAux)) {
+            array_push($arrayCompleta3, $arrayAux[$j]);
+            $j += 1;
+            $i += 1;  
+        }   
+    }
+    foreach ($arrayCompleta3 as $i ) 
+        echo $i . "<br>";
 
 ?>
 </BODY>
